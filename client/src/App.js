ba6d8705191cf0a39fb7dec3be6532907fcadec2
lucas-bresson <contact.bresson@gmail.com>
2020-05-29 19:31:27 +0200

    react & apollo setup

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import Launches from './components/Launches';
+import logo from './logo.svg';
+import './App.css';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="container">
+        <img
+          src={logo}
+          alt="SpaceX"
+          style={{ width: 300, display: 'block', margin: 'auto' }}
+        />
+        <Launches />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
